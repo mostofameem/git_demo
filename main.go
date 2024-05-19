e876b2035621137af58a450191f9f8c857b47652
mostofameem <mostofameem@gmail.com>
2024-05-19 17:12:26 +0600

    veersion 3

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	X := 5
+	Y := 6
+	fmt.Println(X + Y)
 	fmt.Println("KI akta obostha")
 	print()
 }
 func print() {
 	fmt.Println("hi this  is  version 2")
+	fmt.Println("Now try differently")
+	fmt.Println("assada")
 }
